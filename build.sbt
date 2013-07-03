250e722890055c4b07cb1be0824de37c44e63472
Denis Anfertev <danfertev@gmail.com>
2013-07-03 18:30:15 +0400

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "used-kittens"
+
+version := "1.0"
+
+libraryDependencies ++= Seq(
+	"junit" % "junit" % "4.11" % "test",
+	"org.specs2" %% "specs2" % "1.14" % "test"
+)
---
+++ src/main/scala/logic.scala
@@ -0,0 +1,7 @@
+object Logic {
+  def matchLiklihood(kitten: Kitten, buyer: BuyerPreferences): Double = {
+    val matches = buyer.attributes map { attribute => kitten.attributes contains attribute }
+    val nums = matches map { b => if(b) 1.0 else 0.0 }
+    nums.sum / nums.length
+  }
+}
